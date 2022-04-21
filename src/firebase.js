688df19e29b93d8b7df28f0a65250b65edd8b84f
Susanto <nerdyspook007@gmail.com>
2022-04-21 15:28:49 +0530

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD1D0gUnswKEq7Q_3iIz2QEagjh51wl4fk",
+    authDomain: "clone-61a70.firebaseapp.com",
+    projectId: "clone-61a70",
+    storageBucket: "clone-61a70.appspot.com",
+    messagingSenderId: "626352497142",
+    appId: "1:626352497142:web:56f4a184acfb476f2fe7d7",
+};
+
+firebase.initializeApp(firebaseConfig);
